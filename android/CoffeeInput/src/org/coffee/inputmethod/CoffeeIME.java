3b345852b3de5e9a6a10f922ce1945773473f4d6
wangtaomsn@gmail.com <wangtaomsn@gmail.com@c0a0aa5f-09ee-c1f1-287d-fc784bacdd97>
2013-04-19 10:24:36 +0000

    修改获取以及设置输入法的方式

--- android/CoffeeInput/src/org/coffee/inputmethod/CoffeeIME.java
+++ android/CoffeeInput/src/org/coffee/inputmethod/CoffeeIME.java
@@ -1,7 +1,13 @@
 package org.coffee.inputmethod;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.inputmethod.InputMethodInfo;
+import android.view.inputmethod.InputMethodManager;
 
 public class CoffeeIME extends Activity {
 	/** Called when the activity is first created. */
@@ -9,11 +15,27 @@ public class CoffeeIME extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		this.setContentView(R.layout.main);
 
 		// CoffeeIMEService cs = getSystemService(INPUT_METHOD_SERVICE)
 //		Settings.Secure.putString(getContentResolver(),
-//		Settings.Secure.DEFAULT_INPUT_METHOD,
+//				Settings.Secure.DEFAULT_INPUT_METHOD,
 //				"org.coffee.inputmethod/.CoffeeIMEService");
+		onCreateIMM();
 	}
+
+	private Object mInputMethodProperties;
+
+	private void onCreateIMM() {
+		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		List<InputMethodInfo> imList = imm.getInputMethodList();
+		for (InputMethodInfo imi : imList) {
+			System.out.println(imi.getId());
+			// InputMethodService.
+		}
+		imm.setInputMethod(findViewById(R.id.test).getWindowToken(),"org.coffee.inputmethod/.CoffeeIMEService");
+		String mLastInputMethodId = Settings.Secure.getString(
+				getContentResolver(), Settings.Secure.DEFAULT_INPUT_METHOD);
+
+	}
+
 }
